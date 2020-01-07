579da49777ac25d301f6e6ba3c42591e2bf008ed
ashanuoc <ashanuofl@gmail.com>
2020-01-07 16:11:05 -0500

    first problem in python

---
+++ problem_1.py
@@ -0,0 +1,18 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+import numpy as np
+import time as t
+
+tic = t.time()
+
+n = 1000
+
+total = np.sum(np.arange(0, n, 3)) + np.sum(np.arange(0, n, 5)) - np.sum(np.arange(0, n, 15))
+
+print(total)
+
+toc = t.time()
+
+print(toc - tic)
