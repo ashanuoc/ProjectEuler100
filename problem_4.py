f21ca9305a6c39dc45ff375ba74a109014d34299
ashanuoc <ashanuofl@gmail.com>
2020-01-22 12:53:42 -0500

    testing for 3 digit number

--- problem_4.py
+++ problem_4.py
@@ -1,14 +1,25 @@
-num1 = 99
-num2 = 99
+num1 = 999
+num2 = 999
+list = []
+
+while num1 < 1000:
+    while num2 < 1000:
+        number = num1 * num2
+        temp = str(number)
+        reverse = int(''.join(reversed(temp))) #reversed elements and can join with join function
+        if number == reverse :
+            list.append(number)
+            break
+        else:
+            num2 = num2 -1
+    num1 = num1 - 1
+    num2 = num2 -1
 
 
-while num2 < 100:
-    number = num1 * num2
-    temp = str(number)
-    reverse = int(''.join(reversed(temp))) #reversed elements and can join with join function
-    if number == reverse :
-        print('largest palindrome = ', number)
-        break
-    else:
-        num2 = num2 -1
 
+list=[]
+list.append(99)
+print(list)
+list.append(89)
+print(list)
+print(max(list))
