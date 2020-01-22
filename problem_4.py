f18eecefa9996ef69f3e0d363c5321f609c154a6
ashanuoc <ashanuofl@gmail.com>
2020-01-22 12:17:29 -0500

    reversed the string

---
+++ problem_4.py
@@ -0,0 +1,8 @@
+num1 = 99
+num2 = 99
+
+# while True:
+temp = 'ashan' #str(num1 * num2)
+length_temp = len(temp)
+re = ''.join(reversed(temp)) #reversed elements and can join with join function
+print(re)
