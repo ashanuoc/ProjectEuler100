31e9572b1cbb83b9b4fd858bfac94b0968aaa687
ashanuoc <ashanuofl@gmail.com>
2020-01-22 12:33:49 -0500

    two digit palindrome

--- problem_4.py
+++ problem_4.py
@@ -1,8 +1,14 @@
 num1 = 99
 num2 = 99
 
-# while True:
-temp = 'ashan' #str(num1 * num2)
-length_temp = len(temp)
-re = ''.join(reversed(temp)) #reversed elements and can join with join function
-print(re)
+
+while num2 < 100:
+    number = num1 * num2
+    temp = str(number)
+    reverse = int(''.join(reversed(temp))) #reversed elements and can join with join function
+    if number == reverse :
+        print('largest palindrome = ', number)
+        break
+    else:
+        num2 = num2 -1
+
