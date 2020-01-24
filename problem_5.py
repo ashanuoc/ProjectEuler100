fdbb977e25663b2b610ace63eff1f5a4a93f8d3b
ashanuoc <ashanuofl@gmail.com>
2020-01-24 11:11:15 -0500

    final solution

--- problem_5.py
+++ problem_5.py
@@ -1,12 +1,6 @@
-# def smallest_positive_number(num1,num2):
-    
-#     while True:
-#         if num2%num1 == 0 & num:
-#             smallest_positive_number(num1+1,num2)
-#         else:
-#             num2 = num2 + 1
 
-num1 = y = 10
+
+num1 = y = 20
 num2 = 1
 while True:
     if num1%num2 == 0:
@@ -18,9 +12,3 @@ while True:
     else:
         num1 = num1 + 1
         num2 = 1
-
-    
-# print(num1)
-
-
-# print(num1)
