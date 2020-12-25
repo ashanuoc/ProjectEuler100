584fc3ea5b9e694203f88649bb6f39bd32cf8663
ashanuoc <ashanuofl@gmail.com>
2020-12-25 16:37:25 -0500

    Find the largest natural number that its square close to 1000

--- problem_9.py
+++ problem_9.py
@@ -1,6 +1,12 @@
 """ A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-    a2 + b2 = c2
+    a**2 + b**2 = c**2
     For example, 32 + 42 = 9 + 16 = 25 = 52.
 
     There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     Find the product abc."""
+
+import math
+
+maxInt = math.floor(math.sqrt(1000))  # Find the largest natural number that its square close to 1000
+
+print(maxInt)
