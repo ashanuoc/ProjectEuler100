1515dbc00dcba7e18a316e1ac725295e400be1aa
ashanuoc <ashanuofl@gmail.com>
2021-01-06 13:37:40 -0500

    find a number is prime or not

--- problem_10.py
+++ problem_10.py
@@ -1,2 +1,12 @@
 """ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
     Find the sum of all the primes below two million. """
+
+num = 205
+
+for n in range(2, num):
+    s = num%n
+    print('n',n)
+    if s == 0:
+        print('not a prime')
+        break
+
